dbd3e8eec32df087ff0f0c4c334bd405c2351b5b
Vigneshwaran s <115530894+vigneshwaran100@users.noreply.github.com>
2024-07-27 19:15:10 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length):
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    if length < 8:
+        print("Password length should be at least 8 characters.")
+        return None
+    password = ''.join(random.choice(all_characters) for i in range(length))
+    return password
+
+length = int(input("Enter length of the password: "))
+print("Generated Strong Password : ", generate_password(length))
